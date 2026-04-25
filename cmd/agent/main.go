e18a8951fe393190f36f114f7ca8a45512d8675a
developer <otl@fivegen.ru>
2026-04-25 18:34:43 +0300

    refactoring

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,84 +11,10 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/Radiushina/metrics-receiver.git/internal/agent"
 	"github.com/Radiushina/metrics-receiver.git/internal/logger"
 	models "github.com/Radiushina/metrics-receiver.git/internal/model"
 	"github.com/go-resty/resty/v2"
 )
-
-func reportOnce(
-	client *resty.Client,
-	baseURL string,
-	snapshot map[string]float64,
-	delta int64,
-) error {
-	logger.Log.Sugar().Infof(
-		"report: sending %d gauges + RandomValue + PollCount(+%d)…",
-		len(models.GaugeNames),
-		delta,
-	)
-
-	for _, name := range models.GaugeNames {
-		if err := agent.PostMetric(
-			client,
-			baseURL,
-			name,
-			models.Gauge,
-			snapshot[name],
-		); err != nil {
-			logger.Log.Sugar().Warnf(
-				"failed to send gauge %s=%v: %v",
-				name,
-				snapshot[name],
-				err,
-			)
-		}
-	}
-
-	if err := agent.PostMetric(
-		client,
-		baseURL,
-		"RandomValue",
-		models.Gauge,
-		snapshot["RandomValue"],
-	); err != nil {
-		logger.Log.Sugar().Warnf(
-			"failed to send gauge RandomValue=%v: %v",
-			snapshot["RandomValue"],
-			err,
-		)
-	}
-
-	if err := agent.PostIntMetric(
-		client,
-		baseURL,
-		models.PollCount,
-		models.Counter,
-		delta,
-	); err != nil {
-		logger.Log.Sugar().Warnf("failed to send counter PollCount+=%d: %v", delta, err)
-		return err
-	}
-	return nil
-}
-
-func takeReportSnapshot(
-	mu *sync.Mutex,
-	gaugeValues map[string]float64,
-	pollCountDelta *int64,
-) (snapshot map[string]float64, delta int64) {
-	delta = atomic.SwapInt64(pollCountDelta, 0)
-
-	mu.Lock()
-	snapshot = make(map[string]float64, len(gaugeValues))
-	for k, v := range gaugeValues {
-		snapshot[k] = v
-	}
-	mu.Unlock()
-
-	return snapshot, delta
-}
 
 func main() {
 	flags := NewFlags()
@@ -114,16 +40,7 @@ func main() {
 	var pollCountDelta int64
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	poll := func() {
-		runtime.ReadMemStats(&ms)
-		mu.Lock()
-		models.UpdateGaugesFromMemStats(gaugeValues, &ms, rnd)
-		mu.Unlock()
-		atomic.AddInt64(&pollCountDelta, 1)
-		logger.Log.Info("poll: MemStats + RandomValue updated")
-	}
-
-	poll()
+	pollOnce(&ms, &mu, gaugeValues, rnd, &pollCountDelta)
 	logger.Log.Sugar().Infof(
 		"poll: initial update done; first metric report in %v",
 		reportInterval,
@@ -137,7 +54,7 @@ func main() {
 
 	go func() {
 		for range pollTicker.C {
-			poll()
+			pollOnce(&ms, &mu, gaugeValues, rnd, &pollCountDelta)
 		}
 	}()
 
---
+++ cmd/agent/utils.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+
+	"github.com/Radiushina/metrics-receiver.git/internal/agent"
+	"github.com/Radiushina/metrics-receiver.git/internal/logger"
+	models "github.com/Radiushina/metrics-receiver.git/internal/model"
+	"github.com/go-resty/resty/v2"
+)
+
+func reportOnce(
+	client *resty.Client,
+	baseURL string,
+	snapshot map[string]float64,
+	delta int64,
+) error {
+	logger.Log.Sugar().Infof(
+		"report: sending %d gauges + RandomValue + PollCount(+%d)…",
+		len(models.GaugeNames),
+		delta,
+	)
+
+	for _, name := range models.GaugeNames {
+		if err := agent.PostMetric(
+			client,
+			baseURL,
+			name,
+			models.Gauge,
+			snapshot[name],
+		); err != nil {
+			logger.Log.Sugar().Warnf(
+				"failed to send gauge %s=%v: %v",
+				name,
+				snapshot[name],
+				err,
+			)
+		}
+	}
+
+	if err := agent.PostMetric(
+		client,
+		baseURL,
+		"RandomValue",
+		models.Gauge,
+		snapshot["RandomValue"],
+	); err != nil {
+		logger.Log.Sugar().Warnf(
+			"failed to send gauge RandomValue=%v: %v",
+			snapshot["RandomValue"],
+			err,
+		)
+	}
+
+	if err := agent.PostIntMetric(
+		client,
+		baseURL,
+		models.PollCount,
+		models.Counter,
+		delta,
+	); err != nil {
+		logger.Log.Sugar().Warnf("failed to send counter PollCount+=%d: %v", delta, err)
+		return err
+	}
+	return nil
+}
+
+func takeReportSnapshot(
+	mu *sync.Mutex,
+	gaugeValues map[string]float64,
+	pollCountDelta *int64,
+) (snapshot map[string]float64, delta int64) {
+	delta = atomic.SwapInt64(pollCountDelta, 0)
+
+	mu.Lock()
+	snapshot = make(map[string]float64, len(gaugeValues))
+	for k, v := range gaugeValues {
+		snapshot[k] = v
+	}
+	mu.Unlock()
+
+	return snapshot, delta
+}
+
+func pollOnce(
+	ms *runtime.MemStats,
+	mu *sync.Mutex,
+	gaugeValues map[string]float64,
+	rnd *rand.Rand,
+	pollCountDelta *int64,
+) {
+	runtime.ReadMemStats(ms)
+
+	mu.Lock()
+	models.UpdateGaugesFromMemStats(gaugeValues, ms, rnd)
+	mu.Unlock()
+
+	atomic.AddInt64(pollCountDelta, 1)
+	logger.Log.Info("poll: MemStats + RandomValue updated")
+}
