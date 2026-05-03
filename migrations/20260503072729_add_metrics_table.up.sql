ffcdf2ecec0cc8cdf6c269322cf36978a126134c
developer <otl@fivegen.ru>
2026-05-03 12:11:07 +0300

    refactoring tables gauges & counters

--- migrations/20260503072729_add_metrics_table.up.sql
+++ migrations/20260503072729_add_metrics_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS gauges (
-    id      uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
-    name    text NOT NULL,
-    value   DOUBLE PRECISION,
+    id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name    TEXT NOT NULL UNIQUE,
+    value   DOUBLE PRECISION NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS counters (
-    id      uuid  PRIMARY KEY DEFAULT gen_random_uuid(),
-    name    text NOT NULL,
-    value   BIGINT,
+    id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name    TEXT NOT NULL UNIQUE,
+    value   BIGINT  NOT NULL DEFAULT 0
 );
